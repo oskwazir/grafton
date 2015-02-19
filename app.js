3118a91c95acc936d4a9f2c7ef5d759be6450068
Omer Wazir <omer.wazir@gmail.com>
2015-02-19 14:42:32 -0700

    Database works

---
+++ app.js
@@ -0,0 +1,10 @@
+const db = require('./db');
+db.put('key 1', 'value 1');
+db.put('key 2', 'value 2');
+
+db.get('key 1', function(err, value){
+  if(err) return handleError(err);
+  console.log(value);
+})
+
+db.del('key 1');
