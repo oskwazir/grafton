b18ce10c3902570d11767b1b532bbd905bebedde
Omer Wazir <omer.wazir@gmail.com>
2015-02-19 18:39:13 -0700

    Added encoding example

---
+++ encodings/app.js
@@ -0,0 +1,23 @@
+const db = require('./db');
+const leviathan= new Buffer('leviathan','utf8').toString('hex');
+const leviathanText =  new Buffer(`Man is distinguished, not only by his reason, but by this singular passion from
+other animals, which is a lust of the mind, that by a perseverance of delight
+in the continued and indefatigable generation of knowledge, exceeds the short
+vehemence of any carnal pleasure.`, 'utf8').toString('base64');
+
+db.put(leviathan, leviathanText, function(err){
+  if(err) {
+    console.log(err);
+    return;
+  }
+  db.get(leviathan, function(err, value){
+    if(err) {
+      console.error(err);
+      return;
+    }
+    
+    console.log(`Decoded-base-64: ${value}`);
+
+  });
+});
+
---
+++ encodings/db.js
@@ -0,0 +1,11 @@
+const options = {
+  keyEncoding: 'hex',
+  valueEncoding: 'base64'
+}
+const level = require('level');
+const path = require('path');
+
+const dbPath = process.env.DB_PATH || path.join(__dirname,'graftonDB');
+const db = level(dbPath, options);
+
+module.exports = db;
