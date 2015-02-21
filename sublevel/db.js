36a39656162e4fc92f50c57f611635e1f1a69755
Omer Wazir <omer.wazir@gmail.com>
2015-02-21 15:23:30 -0700

    Use sublevels for databases

--- sublevel/db.js
+++
@@ -1,7 +0,0 @@
-const level = require('level');
-const path = require('path');
-
-const dbPath = process.env.DB_PATH || path.join(__dirname,'graftonDB');
-const db = level(dbPath);
-
-module.exports = db;
---
+++ sublevel/sublevels.js
@@ -0,0 +1,12 @@
+const level = require('level');
+const path = require('path');
+const SubLevel = require('level-sublevel');
+
+const dbPath = process.env.DB_PATH || path.join(__dirname,'graftonDB');
+const db = SubLevel(level(dbPath,{
+    valueEncoding:'json'
+}));
+
+exports.base = db;
+exports.users = db.sublevel('users');
+exports.messages = db.sublevel('messages');
