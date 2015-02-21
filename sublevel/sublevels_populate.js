1140f472111f4a016d8c218c5cde0bd2e8c81fca
Omer Wazir <omer.wazir@gmail.com>
2015-02-21 15:23:48 -0700

    Populate sublevels with example data

---
+++ sublevel/sublevels_populate.js
@@ -0,0 +1,26 @@
+const cuid = require('cuid');
+const db = require('./sublevels');
+
+const user = {
+    name:'Foo Manchu',
+    email:'user1@example.com'
+};
+
+const createMessages = function createMessages(){
+    'use strict';
+    for(var i = 0; i < 20;i+=1){
+        var userMessages = db.messages.sublevel(user.email);
+        userMessages.put(cuid(),{
+            from:`user${i}@example.com`,
+            to:user.email,
+            subject:'Hey!',
+            body:'How you doing buddy?'
+        });
+    }
+};
+
+db.users.put(user.email,user,function(err){
+    'use strict';
+    if(err){throw err;}
+    createMessages();
+});
