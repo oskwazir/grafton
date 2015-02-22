322bc6bc79eeb7c0648f14968446fdbd98c34a5b
Omer Wazir <omer.wazir@gmail.com>
2015-02-21 17:07:43 -0700

    Randomize user message accounts

--- sublevel/sublevels_populate.js
+++ sublevel/sublevels_populate.js
@@ -2,16 +2,20 @@
 const db = require('./sublevels');
 const inboxes = require('./sublevels_insert_message');
 
+const getNewUserID = function getNewUserID(){
+    'use strict';
+    return Math.floor(Math.random() * 100)
+}
 const user = {
     name:'Foo Manchu',
-    email:'user1@example.com'
+    email:`user1@example.com`
 };
 
 const createMessages = function createMessages(){
     'use strict';
-    for(var i = 0; i < 20;i+=1){
+    for(var i = 0; i < 5;i+=1){
         //var userMessages = db.messages.sublevel(user.email);
-        inboxes.insert({from:`user${i}@example.com`,
+        inboxes.insert({from:`user${getNewUserID()}@example.com`,
                           to:user.email,
                           subject:'Hey!',
                           body:'How you doing buddy?'});
